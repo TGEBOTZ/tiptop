57f4e50278872ccd68d0e09834b62b41321f5c92
𝗧𝗚𝗘𝗕𝗢𝗧𝗭 <97307696+TGEBOTZ@users.noreply.github.com>
2026-07-13 20:30:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,24 @@
-const prizes = [
-"❤️ Give Mom a hug!",
-"🍫 You win a chocolate!",
-"☕ Make tea for your family!",
-"📸 Take a family selfie!",
-"🍕 Pizza treat for everyone!",
-"😂 Tell a funny joke!",
-"🎬 Family movie night!",
-"🎁 You get a surprise gift!",
-"⭐ You are the lucky winner today!"
-];
-
-
-let lastPrize = -1;
-
-
-function playGame(number){
-
-document.getElementById("number").innerHTML =
-"Opening Tip Top... ⏳";
-
-
-setTimeout(function(){
-
-
-let prize;
-
-
-do{
-
-prize = Math.floor(Math.random()*prizes.length);
-
+if (localStorage.getItem("played") === "yes") {
+    document.getElementById("msg").innerHTML =
+    "You already played this game!";
+    
+    document.querySelectorAll("button").forEach(btn => {
+        btn.disabled = true;
+    });
 }
-while(prize === lastPrize);
 
+function play(number) {
 
-lastPrize = prize;
+    if(localStorage.getItem("played") === "yes"){
+        return;
+    }
 
+    localStorage.setItem("played","yes");
 
-document.getElementById("number").innerHTML =
-"Your Number: " + number;
+    document.getElementById("msg").innerHTML =
+    "Your selected number is: " + number;
 
-
-document.getElementById("result").innerHTML =
-prizes[prize];
-
-
-},1000);
-
-
+    document.querySelectorAll("button").forEach(btn => {
+        btn.disabled = true;
+    });
 }
