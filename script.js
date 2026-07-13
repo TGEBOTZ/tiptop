c47be9b529d4709d0997d533640f690a22150ab8
𝗧𝗚𝗘𝗕𝗢𝗧𝗭 <97307696+TGEBOTZ@users.noreply.github.com>
2026-07-13 20:57:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 const prizes=[
-"Free Lunch",
-"Coffee Voucher",
-"Gift Item",
-"Movie Ticket",
-"Mobile Recharge",
-"Chocolate Box",
-"Shopping Voucher",
-"Surprise Gift",
-"Better Luck Next Time"
+"Gift A",
+"Gift B",
+"Gift C",
+"Gift D",
+"Gift E",
+"Gift F",
+"Gift G",
+"Gift H",
+"Gift I"
 ];
+
+
+function sound(){
+
+let ctx=new AudioContext();
+
+let osc=ctx.createOscillator();
+
+osc.frequency.value=800;
+
+osc.connect(ctx.destination);
+
+osc.start();
+
+setTimeout(()=>{
+osc.stop();
+},200);
+
+}
+
 
 
 function play(number,btn){
 
 btn.classList.add("active");
 
-
-let sound=document.getElementById("sound");
-sound.play();
+sound();
 
 
-let prize=
+let prize =
 prizes[Math.floor(Math.random()*prizes.length)];
+
+
+confetti();
 
 
 setTimeout(()=>{
 
-document.getElementById("result").innerHTML=
+document.getElementById("prize").innerHTML=
 "Your Number : "+number+
-"<br><br>🎁 Prize : "+prize;
+"<br><br>🎁 "+prize;
 
 
 document.getElementById("popup").style.display="block";
@@ -38,6 +59,36 @@ btn.classList.remove("active");
 
 
 },1000);
+
+
+}
+
+
+
+function confetti(){
+
+for(let i=0;i<80;i++){
+
+let c=document.createElement("div");
+
+c.className="confetti";
+
+c.style.left=Math.random()*100+"%";
+
+c.style.background=
+"rgb("+Math.random()*255+","+
+Math.random()*255+","+
+Math.random()*255+")";
+
+
+document.body.appendChild(c);
+
+
+setTimeout(()=>{
+c.remove();
+},3000);
+
+}
 
 }
 
